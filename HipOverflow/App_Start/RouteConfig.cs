418ad3b5d9caf978c0cc976973cd2359fb5d5ae1
SK <SK@DESKTOP-ODF2G47>
2021-02-05 16:25:28 +0400

    Adding routes and models

--- HipOverflow/App_Start/RouteConfig.cs
+++ HipOverflow/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace HeapOverflow
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+             name: "Login",
+             url: "{controller}",
+             defaults: new { controller = "Login", action = "Index" }
+         );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+          
         }
     }
 }
