eddc18fa053dd64607da6b080356c70350bf63b5
sakheli <morisi1999@gmail.com>
2021-02-05 18:07:01 +0400

    User part of service mostly done

--- WCF/Service1.svc.cs
+++ WCF/Service1.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using WCF.DataContract;
 
 namespace WCF
 {
@@ -12,22 +13,44 @@ namespace WCF
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public bool AddPost(PostContract post, CategoryContract category, int userId)
         {
-            return string.Format("You entered: {0}", value);
+            return BusinessLogic.BusinessLogic.AddPost(post, category, userId);
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public bool AddReply(PostContract post, ReplyContract reply, UserContract user)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return BusinessLogic.BusinessLogic.AddReply(post, reply, user);
+        }
+
+        public bool DeletePost(int userId, PostContract post)
+        {
+            return BusinessLogic.BusinessLogic.DeletePost(userId, post);
+        }
+
+        public PostContract GetPost(int id)
+        {
+            return BusinessLogic.BusinessLogic.GetPost(id);
+        }
+
+        public List<PostContract> GetPosts(CategoryContract category)
+        {
+            return BusinessLogic.BusinessLogic.GetPosts(category);
+        }
+
+        public List<PostContract> GetPostsByUserId(int id)
+        {
+            return BusinessLogic.BusinessLogic.GetPostsByUserId(id);
+        }
+
+        public UserContract Login(string email, string password)
+        {
+            return BusinessLogic.BusinessLogic.Login(email, password);
+        }
+
+        public bool Register(UserContract user)
+        {
+            return BusinessLogic.BusinessLogic.Register(user);
         }
     }
 }
